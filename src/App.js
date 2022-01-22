7fc10f86a7af78f0e4b74c680c96cb438a36d1f9
samnj <7932916+samnj@users.noreply.github.com>
2022-01-22 18:23:13 -0300

    refactored components

--- src/App.js
+++ src/App.js
@@ -1,49 +1,10 @@
 import React, { useState } from 'react'
 import { Routes, Route, Link } from 'react-router-dom'
 
-const Menu = () => {
-  const padding = {
-    paddingRight: 5
-  }
-  return (
-    <div>
-      <Link style={padding} to='/'>anecdotes</Link>
-      <Link style={padding} to='/create'>create new</Link>
-      <Link style={padding} to='/about'>about</Link>
-    </div>
-  )
-}
-
-const AnecdoteList = ({ anecdotes }) => (
-  <div>
-    <h2>Anecdotes</h2>
-    <ul>
-      {anecdotes.map(anecdote => <li key={anecdote.id} >{anecdote.content}</li>)}
-    </ul>
-  </div>
-)
-
-const About = () => (
-  <div>
-    <h2>About anecdote app</h2>
-    <p>According to Wikipedia:</p>
-
-    <em>An anecdote is a brief, revealing account of an individual person or an incident.
-      Occasionally humorous, anecdotes differ from jokes because their primary purpose is not simply to provoke laughter but to reveal a truth more general than the brief tale itself,
-      such as to characterize a person by delineating a specific quirk or trait, to communicate an abstract idea about a person, place, or thing through the concrete details of a short narrative.
-      An anecdote is "a story with a point."</em>
-
-    <p>Software engineering is full of excellent anecdotes, at this app you can find the best and add more.</p>
-  </div>
-)
-
-const Footer = () => (
-  <div>
-    Anecdote app for <a href='https://courses.helsinki.fi/fi/tkt21009'>Full Stack -websovelluskehitys</a>.
-
-    See <a href='https://github.com/fullstack-hy/routed-anecdotes/blob/master/src/App.js'>https://github.com/fullstack-hy2019/routed-anecdotes/blob/master/src/App.js</a> for the source code.
-  </div>
-)
+import About from './components/About'
+import AnecdoteList from './components/AnecdoteList'
+import Footer from './components/Footer'
+import Menu from './components/Menu'
 
 const CreateNew = (props) => {
   const [content, setContent] = useState('')
